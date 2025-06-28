74a6171f800f5416bbaf6953862f113e631849ab
Gabrielly <gabicasstilho@gmail.com>
2025-06-28 23:08:32 +0100

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+import Header from "./components/Header";
+import ConviteTexto from "./components/ConviteTexto";
+import BotoesAcoes from "./components/BotoesAcoes";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ConviteTexto />
+      <BotoesAcoes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
