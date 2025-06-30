45b02669ffd8f8bf2e9863a47d4cece833182afe
Gabrielly <gabicasstilho@gmail.com>
2025-06-30 16:44:14 +0100

    data e local

--- src/components/ConviteDataLocal/ConviteDataLocal.jsx
+++ src/components/ConviteDataLocal/ConviteDataLocal.jsx
@@ -1,2 +1,26 @@
+import "./ConviteDataLocal.css";
+import MolduraCima from "../../assets/svg/molduracimadatalocal.svg";
+import MolduraBaixo from "../../assets/svg/moldurabaixodatalocal.svg";
+
+export default function ConviteDataLocal() {
+  return (
+    <div className="convite-container">
+      <div className="data">
+         <img className="Moldura cima" src={MolduraCima} alt="Moldura cima" />
+        <p className="mes">NOVEMBRO</p>
+        <h1>
+          <span className="sabado">SÁB |</span>
+          <span className="dia">01</span>
+          <span className="sete-horas">| 19:00</span>
+        </h1>
+        <p className="ano">2025</p>
+      </div>
+      <p className="local">
+        Espaço Cristallis: R. Dr. João Carlos Azevedo, Vila Bandeirantes, Mauá - SP
+      </p>
+      <img className="Moldura baixo" src={MolduraBaixo} alt="Moldura baixo" />
+    </div>
+  );
+}
 
 
